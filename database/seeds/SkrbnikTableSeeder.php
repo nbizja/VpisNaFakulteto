57c8db6a6dba097df294c0fb5d8f29eeead6ca7b
Nejc Bizjak <nejc.bizjak@1click2sport.com>
2016-04-04 15:49:39 +0200

    Dodan en uporabnik, skrbnik in administrator.

--- database/seeds/SkrbnikTableSeeder.php
+++ database/seeds/SkrbnikTableSeeder.php
@@ -11,15 +11,26 @@ class SkrbnikTableSeeder extends Seeder
 {
     public function run()
     {
-        DB::table('uporabnik')->insert([
-            'id' => 1,
-            'ime' => 'Skrba',
-            'priimek' => 'Brba',
-            'username' => 'skrbnik',
-            'email' => 'skrbnik@faks.me',
-            'password' => bcrypt('skrbnik'),
-            'vloga' => VlogaUporabnika::ADMIN
-        ]);
+        DB::table('uporabnik')->insert(
+            [
+                'id' => 1,
+                'ime' => 'Skrba',
+                'priimek' => 'Brba',
+                'username' => 'skrbnik',
+                'email' => 'skrbnik@faks.me',
+                'password' => bcrypt('skrbnik'),
+                'vloga' => VlogaUporabnika::SKRBNIK_PROGRAMA
+            ],
+            [
+                'id' => 2,
+                'ime' => 'Takozvani',
+                'priimek' => 'Gazda',
+                'username' => 'admin',
+                'email' => 'admin@faks.me',
+                'passowrd' => bcrypt('admin'),
+                'vloga' => VlogaUporabnika::ADMIN
+            ]
+        );
     }
 
 }
