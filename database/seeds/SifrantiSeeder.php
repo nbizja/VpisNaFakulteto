390aee4070023c5e166f05dadf8dc4fb9c9e2f9d
Nejc Bizjak <nejc.bizjak@1click2sport.com>
2016-04-04 09:22:36 +0200

    Popravljeni seederji. Seede za sifrante sem premaknil iz DatabaseSeeder v SifrantiSeeder (se vedno ne dela).

---
+++ database/seeds/SifrantiSeeder.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace database\seeds;
+
+use Flynsarmy\CsvSeeder\CsvSeeder;
+use Illuminate\Support\Facades\DB;
+
+
+/**
+ * Seeds database with data from csv files.
+ *
+ * Names of data files must exactly match the names of tables.
+ * The leading line of each data file must be a header.
+ * Names of colums in data file must exactly match colums in database
+ * table.
+ */
+class SifrantiSeeder extends CsvSeeder
+{
+    /**
+     * Path to a directory containing data files.
+     */
+    const DATA_DIR = '/database/seeds/sifranti/';
+
+    /**
+     * Extension of datafiles in `DATA_DIR`, including the leading
+     * dot.'
+     */
+    const DATA_EXTENSION = '.csv';
+
+    /**
+     * Initialize the DatabaseSeeder object. Searches for all data
+     * files in `DATA_DIR` and saves their paths in `$file_list`.
+     *
+     * @return void
+     */
+     public function __construct()
+     {
+         $this->file_list = array_filter(
+             glob(base_path() . self::DATA_DIR . '*' . self::DATA_EXTENSION),
+             'is_file');
+     }
+
+    /**
+     * Seeds a table for each data file in `DATA_DIR`.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $this->call(SkrbnikTableSeeder::class);
+        DB::disableQueryLog();
+        foreach ($this->file_list as $path) {
+            $this->table = basename($path, self::DATA_EXTENSION);
+            $this->filename = $path;
+
+            parent::run();
+        }
+    }
+}
+
