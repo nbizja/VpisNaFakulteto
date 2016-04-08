f42b697c6c4a53e98db0362fccaa812c2abcbb8d
Primož Belej <pbelej90@gmail.com>
2016-04-08 19:39:23 +0200

    Dodal migracije za dodajanje stolpca sifra v studijski_program. Nastavitev FK skrbnika v visokosolski_zavod na nullable.

---
+++ database/migrations/2016_04_08_152347_make_skrbnik_nullable_in_visokosolski_zavod.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class MakeSkrbnikNullableInVisokosolskiZavod extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+		DB::statement('SET FOREIGN_KEY_CHECKS = 0;');
+        Schema::table('visokosolski_zavod', function (Blueprint $table) {
+            $table->integer('id_skrbnika')->nullable()->change();
+        });
+		DB::statement('SET FOREIGN_KEY_CHECKS = 1;');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('visokosolski_zavod', function (Blueprint $table) {
+            $table->integer('id_skrbnika')->nullable()->change();
+        });
+    }
+}
