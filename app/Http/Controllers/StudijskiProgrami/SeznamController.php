5bec45f86d81f7472fe871df15fed7e21be843f9
Neza Belej <nezabelej@gmail.com>
2016-04-29 10:41:31 +0200

    Delujoca zgodba; manjka izvoz v PDF

--- app/Http/Controllers/StudijskiProgrami/SeznamController.php
+++ app/Http/Controllers/StudijskiProgrami/SeznamController.php
@@ -13,6 +13,7 @@ use App\Models\Repositories\StudijskiProgramiRepository;;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\ThrottlesLogins;
 use Illuminate\Foundation\Auth\AuthenticatesAndRegistersUsers;
+use Illuminate\Http\Request;
 
 class SeznamController extends Controller
 {
@@ -32,10 +33,36 @@ class SeznamController extends Controller
      */
     protected $redirectTo = '/';
 
-    public function seznamProgramov()
+    public function seznamProgramov(Request $request)
     {
         if (Auth::check()) {
             if (Auth::user()->vloga == 'skrbnik') {
+
+                if ($request->query->has('nacin')) {
+                    if ($request->query->get('nacin') == "redni") {
+                        $programi = $this->studijskiProgrami->ProgramiRedni()->sortBy('visokosolskiZavod.ime');
+                    } else {
+                        $programi = $this->studijskiProgrami->ProgramiIzredni()->sortBy('visokosolskiZavod.ime');
+                    }
+                    return view('studijskiProgrami.seznamProgramov', ['programi' => $programi]);
+
+                } else if ($request->query->has('vrsta')) {
+                    if ($request->query->get('vrsta') == "vs") {
+                        $programi = $this->studijskiProgrami->ProgramiVs()->sortBy('visokosolskiZavod.ime');
+                    } else {
+                        $programi = $this->studijskiProgrami->ProgramiUn()->sortBy('visokosolskiZavod.ime');
+                    }
+                    return view('studijskiProgrami.seznamProgramov', ['programi' => $programi]);
+
+                } else if ($request->query->has('omejitev')) {
+                    if ($request->query->get('omejitev') == "da") {
+                        $programi = $this->studijskiProgrami->ProgramiOmejitev()->sortBy('visokosolskiZavod.ime');
+                    } else {
+                        $programi = $this->studijskiProgrami->ProgramiBrezOmejitve()->sortBy('visokosolskiZavod.ime');
+                    }
+                    return view('studijskiProgrami.seznamProgramov', ['programi' => $programi]);
+                }
+
                 $programi = $this->studijskiProgrami->ProgramiAll()->sortBy('visokosolskiZavod.ime');
                 return view('studijskiProgrami.seznamProgramov', ['programi' => $programi]);
             }
