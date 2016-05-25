ec8e2274406a03b8405a059f916b10b8273bc9da
Primož Belej <pbelej90@gmail.com>
2016-05-25 16:29:47 +0200

    Dodano preprosto preverjanje pravilnosti datotek.

--- app/Http/Controllers/Matura/MaturaController.php
+++ app/Http/Controllers/Matura/MaturaController.php
@@ -38,6 +38,9 @@ class MaturaController extends Controller
 
 	private function preberiVrsticoMaturant($vrstica)
 	{
+		if (strlen($vrstica) < 89 || $vrstica[13] != 'Q' || $vrstica[39] != 'Q') {
+			return null;
+		}
 		$emso = trim(substr($vrstica, 0, 13));
 		$ime = trim(substr($vrstica, 14, 25));
 		$ime = iconv('CP1250', 'UTF-8', $ime);
@@ -65,6 +68,9 @@ class MaturaController extends Controller
 	
 	private function preberiVrsticoMaturantPre($vrstica)
 	{
+		if (strlen($vrstica) < 28 || $vrstica[13] != 'Q' || $vrstica[18] != 'Q') {
+			return null;
+		}
 		$emso = trim(substr($vrstica, 0, 13));
 		$id_predmeta = trim(substr($vrstica, 14, 4));
 		$ocena = trim(substr($vrstica, 19, 1));
@@ -102,6 +108,9 @@ class MaturaController extends Controller
 					DB::statement('SET FOREIGN_KEY_CHECKS = 0');
 					foreach(file($maturant) as $line) {
 						$maturant_podatki = $this->preberiVrsticoMaturant($line);
+						if ($maturant_podatki == null) {
+							return back()->withFlashMessage('Napaka v datoteki maturant.');
+						}
 						$model->dodaj($maturant_podatki);
 					}
 					DB::statement('SET FOREIGN_KEY_CHECKS = 1');
@@ -115,6 +124,9 @@ class MaturaController extends Controller
 					DB::statement('SET FOREIGN_KEY_CHECKS = 0');
 					foreach(file($maturantpre) as $line) {
 						$maturantpre_podatki = $this->preberiVrsticoMaturantPre($line);
+						if ($maturantpre_podatki == null) {
+							return back()->withFlashMessage('Napaka v datoteki maturpre.');
+						}
 						$model->dodaj($maturantpre_podatki);
 					}
 					DB::statement('SET FOREIGN_KEY_CHECKS = 1');
