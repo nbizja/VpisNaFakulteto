144959a465c5a037843a935e21d836b0ce077f3d
Nejc Bizjak <nejc@undiq.org>
2016-06-09 21:32:03 +0200

    Dodane označbe za slovence brez slovenskega državljanstva.

--- resources/views/razvrscanje/vsi_rezultati.blade.php
+++ resources/views/razvrscanje/vsi_rezultati.blade.php
@@ -13,6 +13,10 @@
                         <a class="btn btn-primary" href="{{ action('UspehKandidatovController@zapisiTocke') }}">Izracunaj točke</a>
                         <a class="btn btn-primary" href="{{ action('RazvrscanjeController@razvrsti') }}">Razvrsti kandidate</a>
                         <a class="btn btn-primary" href="{{ action('RazvrscanjeController@izvoziSklepe') }}">Izvozi sklepe</a>
+                        <br>
+                        <br>
+                        <br>
+                        <p>*SBSD = Slovenec brez slovenskega državljanstva</p>
                         @foreach($programi as $program)
                             <table class="table table-bordered table-hover table-striped">
                                 <caption>{{ $program->ime . '('. $program->nacin_studija  .')' }}</caption>
@@ -23,9 +27,10 @@
                                     <th>Priimek</th>
                                     <th>Stevilo tock</th>
                                     <th>Želja</th>
+                                    <th>Opombe</th>
                                 </tr>
                                 <tr>
-                                    <td colspan="6">Kandidati s slovenskim državljanstvom</td>
+                                    <td colspan="7">Kandidati iz območja EU</td>
                                 </tr>
                                 <?php $uvrstitevSlo = $uvrstitevTujci =  1; ?>
                                 @foreach($program->prijave->filter(function($prijava) {
@@ -38,17 +43,18 @@
                                         <td>{{ $prijava->kandidat->priimek }}</td>
                                         <td>{{ $prijava->tocke }}</td>
                                         <td>{{ $prijava->zelja }}</td>
+                                        <td></td>
                                     </tr>
                                     <?php $uvrstitevSlo++; ?>
                                 @endforeach
                                 @if($uvrstitevSlo == 1)
                                     <tr>
-                                        <td colspan="6">/</td>
+                                        <td colspan="7">/</td>
                                     </tr>
                                 @endif
 
                                 <tr>
-                                    <td colspan="6">Kandidati brez slovenskega državljanstva</td>
+                                    <td colspan="7">Kandidati izven območja EU</td>
                                 </tr>
                                 @foreach($program->prijave->filter(function($prijava) {
                                             return $prijava->tujec;
@@ -60,12 +66,14 @@
                                         <td>{{ $prijava->kandidat->priimek }}</td>
                                         <td>{{ $prijava->tocke }}</td>
                                         <td>{{ $prijava->zelja }}</td>
+                                        <td>{{ $prijava->osebniPodatki->id_drzavljanstva == 5 ? 'SBSD' : '' }}</td>
+
                                     </tr>
                                     <?php $uvrstitevTujci++; ?>
                                 @endforeach
                                 @if($uvrstitevTujci == 1)
                                     <tr>
-                                        <td colspan="6">/</td>
+                                        <td colspan="7">/</td>
                                     </tr>
                                 @endif
 
